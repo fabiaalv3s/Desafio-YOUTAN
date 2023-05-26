a5f037944eb17c8bea88b348b5e81aaa742a0cac
fabiaalv3s <fabia.k.alves15@gmail.com>
2023-05-26 09:33:34 -0300

    user removed

--- ACME/settings.py
+++ ACME/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'modules.home',
     'modules.clientes',
     'tools.general',
-    'users',
+    # 'users',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +64,6 @@ TEMPLATES = [
         'DIRS': [
             'tools/templates',
             'modules/home/templates',
-            'users/templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -94,20 +93,20 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
 
 
 # Internationalization
--- ACME/urls.py
+++ ACME/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('modules.home.urls')),
     path('clientes', include('modules.clientes.urls')),
-    path('accounts/', include('users.urls')),
+    # path('accounts/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
 
